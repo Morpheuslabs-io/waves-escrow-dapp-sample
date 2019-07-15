06c142a426f5b9344268126f5a07d384a3979cf4
Duong Van Sang <sangdv2703@gmail.com>
2019-07-15 11:32:35 +0700

    Update code & readme file

--- README.md
+++ README.md
@@ -1,58 +1,100 @@
-# Waves Escrow dApp sample with waveJ API 
+# Waves Escrow dApp sample with waveJ API
 
-## Pre-requirements:
-Install these prerequisites:
+## Problem
 
-1. Install Java SDK 1.8 (https://www.oracle.com/technetwork/java/javase/downloads/jdk8-downloads-2133151.html)
-2. Install Maven 3.5+ (https://maven.apache.org/download.cgi)
+We will discuss the problem of buying physical goods with cryptocurrencies. Should the buyer trust the seller and pay before receiving the goods? Should the seller trust the buyer and ship the goods before receiving payment? This problem can be solved by using a third party escrow service. A well-known improvement uses Waves multisignature feature. In this scheme, the money is not sent directly to the escrow service’s address, but instead, it is sent to a 2-of-3 multisig address with one key controlled by each of the transacting parties and one controlled by the mediator. When there is no dispute, the two transacting parties can create together the transfer transaction. Only if there is a dispute the mediator will get involved, collaborating with either the buyer or seller (as appropriate) to redeem the funds.
 
 ## Step 1. Clone the project
 `git clone https://github.com/Morpheuslabs-io/waves-dapp-sample-01`
 
-## Step 2. Update configuration
+## Step 2. Configure Ride IDE
+Open server tab, and then click on waves-ide link to open Ride IDE
+<img src="img/open-ide.png" alt="Ride IDE"/>
+
+Let generate new account to attach dapp script later by clicking generate new account
+<img src="img/ride-ide.png" alt="Ride IDE"/>
+
+After create account has no any waves yet, we will send it some way from genesis account later.
+<img src="img/ride-account.png" alt="Ride account"/>
+
+Then, Open Wave BlockChain Ops and click on retrieve button to get genesis account info, copy seed for later usage.
+<img src="img/blockchainInfo.png" alt="Blockchain Info"/>
+
+Next, let configure Ride IDE to connect to our blockchain ops. Click on setting icon and click add node with 
+External RPC URL from blockchain ops.
+<img src="img/ride-config.png" alt="Blockchain Info"/>
+
+## Step 3. Send Waves and deploy contract script
+
+After finish configuration, it's time to make some transactions.
+Ok, now we send some waves to new account we just created, unfortunately, waves does not provide wallet for development network yes, so we need to use some Ride Console and javascript to do it.
+
+```javascript
+// the seed you just copy above
+const seed = "FwGv7haZrYD5vNSmhYbmBg76fhyGCTatObCKiR3lxnHQVbA9EouYA0Hq4pLNmw7heFbwSJqe9H24uv3XFoID19JWsn8A6Wc2daMo"
+
+// let define transaction object with 10 waves, we send from genesis account 
+// recipient is your account address you just create
+// amount is total waves need to send = waves*10^8
+const txo = {
+"type"4,
+"senderPublicKey":"AGRAhCD5rJUQDG6mE54voaE4et8bdRu6kAenyfPyM6SB",
+"recipient":'3FQ5tmDWAnQkkMNeWy8gYPBBqGy3XUSM7aE', 
+"amount": 1000000000 
+};
+
+// sign transaction with seed
+const signedTx = transfer(txo, seed);
+
+// broadcast transaction to network
+const result = await broadcast(signedTx)
+"transactionId: "+result.id
+
+```
+
+Copy and paste these codes to Ride console, after run code, we get transaction Id, wait little bit to see waves available on your account.
+<img src="img/transfer.png" alt="transfer"/>
+
+Now we will deploy escrow dapp script to this account.
+- Create dapp script by clicking on (+) icon and select dapp script
+- Open file `src/main/resources/script.ride` and past it to Ride IDE
+- Click on Deploy Account Script button
+- Click Add sign button to sign for transaction
+- And then final click on publish to deploy contract.
+<img src="img/deploy-contract.png" alt="transfer"/>
+
+After finish deploy you can check on explorer script is attached to account, script tab.
+<img src="img/explorer.png" alt="explorer"/>
+
+## Step 4. Integrate with smart account with WaveJ API
+ 
+
+### Update configuration
 Open file `src\main\resources\application.properties`
 - Update NODE_URL field to your node url
-- Update NETWORK_BYTE to correct byte mainnet is W, testnet is T
+- Update BUYER_PK with correct private key of address with some waves in it
 - Update BUYER_PK with correct private key of address with some waves in it
 
-## Step 3. Run app
+## Run app
 
 ```
 cd waves-dapp-sample-01
 mvn clean spring-boot:run` to run application
 ```
 
-## Step 4. Check transaction Info
-Access `{node_url}/api-docs/index.html#!/transactions/info/{}` and enter your transaction Id to check transaction on network, example:
+If you view java code you can see, withdraw transaction is sign by both buyer and seller, if there is only one signature, transaction could not make.
 
-Request
+```java
 
-```
-curl -X GET --header 'Accept: application/json' 'https://waves1-20860.morpheuslabs.io/transactions/info/GpdLybwJ9WcbagjBbcQnWt3dZ8XHtr8LtTy3eVjs4BVv'
-```
+    LOG.info("Seller request withraw money");
+    Transaction tx1 = Transaction.makeTransferTx(smartAccount, sellerAddress, 1000000000,"WAVES", fee * 4 ,"WAVES", "Sending Money");
 
-Response
-
-```
-{
-  "senderPublicKey": "hupnQX3JsxmSFMN3kdD972QHfca3q8p9r36QQet7JLR",
-  "amount": 1,
-  "fee": 4000000,
-  "type": 4,
-  "version": 2,
-  "attachment": "BJHsqe5yBQVtpDUdYdwvZv",
-  "sender": "3FhsjnqyDPyXnqJrrGNB47WrjCGygiEiCto",
-  "feeAssetId": null,
-  "proofs": [
-    "5x4nNAzEkxKrnkPmMS2CA7Jv6dzWm8ZusD4ys3WmLdzz99R9pLZESbjSzQ4GenYS24K4JHHNW2AXDZ4Nsr2nx5hZ",
-    "",
-    "ZkC6RwGAtLUH1aVPKpXGP6n7dmhNt84ogDSpDigDs2oPgpzoDK1ymD5wGqe1tmbsVCrQHM1iQ8cXWxqmjH737EE"
-  ],
-  "assetId": null,
-  "recipient": "3FTh7CPm6ZPsUCaUCMjZz37vAgddS4JCcci",
-  "feeAsset": null,
-  "id": "GpdLybwJ9WcbagjBbcQnWt3dZ8XHtr8LtTy3eVjs4BVv",
-  "timestamp": 1562563716971,
-  "height": 12022
-}
+    //Buyer and Seller sign the deal with proofs
+    String buyerSig =  buyer.sign(tx1);
+    LOG.info("Buyer accept transaction by signning it: {}", buyerSig);
+    
+    String sellerSig =  seller.sign(tx1);
+    LOG.info("Seller sign with transaction: {}", sellerSig);
+    tx1 = tx1.withProof(0, buyerSig);
+    tx1 = tx1.withProof(1, sellerSig);
 ```
--- src/main/java/com/mpl/waves/CmdApplicationRunner.java
+++ src/main/java/com/mpl/waves/CmdApplicationRunner.java
@@ -13,6 +13,7 @@ import com.wavesplatform.wavesj.Account;
 import com.wavesplatform.wavesj.Base58;
 import com.wavesplatform.wavesj.Node;
 import com.wavesplatform.wavesj.PrivateKeyAccount;
+import com.wavesplatform.wavesj.PublicKeyAccount;
 import com.wavesplatform.wavesj.Transaction;
 
 @Component
@@ -28,8 +29,8 @@ public class CmdApplicationRunner implements CommandLineRunner{
 	@Value ("${FEE}")
 	private Long fee;
 	
-	@Value("${BUYER_PK}")
-	private String BUYER_PK;
+	@Value("${BUYER_PRIVATEKEY}")
+	private String BUYER_PRIVATEKEY;
 	
 	@Value("${SELLER_SEED}")
 	private String SELLER_SEED;
@@ -37,82 +38,51 @@ public class CmdApplicationRunner implements CommandLineRunner{
 	@Value("${ESCROW_SEED}")
 	private String ESCROW_SEED;
 	
+	@Value("${SMART_ACCOUNT_PUBLIC_KEY}")
+	private String smartAccountPublicKey;
+	
 	@Override
     public void run(String... args) throws IOException, URISyntaxException {
         LOG.info("Starting Escrow smart contract demo");
   
         // Connect to Wave nodes
         Node node = new Node(nodeURL);
+        PublicKeyAccount smartAccount = new PublicKeyAccount(smartAccountPublicKey, (byte)NETWORK_BYTE);
 
         //Get Private keys
-        PrivateKeyAccount buyer = PrivateKeyAccount.fromPrivateKey(BUYER_PK, (byte)NETWORK_BYTE);
+        PrivateKeyAccount buyer = PrivateKeyAccount.fromPrivateKey(BUYER_PRIVATEKEY, (byte)NETWORK_BYTE);
         PrivateKeyAccount seller = PrivateKeyAccount.fromSeed(SELLER_SEED, 0, (byte)NETWORK_BYTE);
-        PrivateKeyAccount escrow= PrivateKeyAccount.fromSeed(ESCROW_SEED,0, (byte)NETWORK_BYTE);
+        PrivateKeyAccount escrow = PrivateKeyAccount.fromSeed(ESCROW_SEED,0, (byte)NETWORK_BYTE);
 
-        //Generating random account
-        String newAccountSeed = PrivateKeyAccount.generateSeed();
-        PrivateKeyAccount newAccount = PrivateKeyAccount.fromSeed(newAccountSeed, 0, (byte)NETWORK_BYTE);
-        String newAccountAddress = newAccount.getAddress();
-        LOG.info("Create New address to deploy script contract: {}", newAccountAddress);
-
-        LOG.info("Send some waves to new address");
-        Transaction tx2 = Transaction.makeTransferTx(buyer, newAccountAddress, 100000000,"WAVES", fee * 4 ,"WAVES", "Sending Waves");
-        String sendingTX = node.send(tx2);
-        
-        LOG.info("Transaction Id: {}", sendingTX);
-
-        try {
-            Thread.sleep(10000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+        String newAccountAddress = smartAccount.getAddress();
+        LOG.info("Smart Contract Address: {}", newAccountAddress);
 
         //Get Addresses and print them
         String buyerAddress = buyer.getAddress();
         String sellerAddress = seller.getAddress();
-        //String escrowAddress = escrow.getAddress();
-        LOG.info("buyerPK: {}", Base58.encode(buyer.getPublicKey()));
-        LOG.info("sellerPK: {}", Base58.encode(seller.getPublicKey()));
-
-        LOG.info("Compile smart contract script");
-        // Set Script and compile it
-        String script = "let buyerPubKey  = base58'" + Base58.encode(buyer.getPublicKey()) + "';" +
-                "let sellerPubKey = base58'" + Base58.encode(seller.getPublicKey()) + "';" +
-                "let escrowPubKey = base58'" + Base58.encode(escrow.getPublicKey()) + "';" +
-                "let buyerSigned = if(sigVerify(tx.bodyBytes, tx.proofs[0], buyerPubKey)) then 1 else 0;" +
-                "let sellerSigned = if(sigVerify(tx.bodyBytes, tx.proofs[1], sellerPubKey)) then 1 else 0;" +
-                "let escrowSigned = if(sigVerify(tx.bodyBytes, tx.proofs[2], escrowPubKey)) then 1 else 0;" +
-                "buyerSigned + sellerSigned + escrowSigned >= 2";
-        String bytecode = node.compileScript(script);
-        LOG.info("contract bytecode: {}", bytecode);
         
-        // new Account Make Script transaction and send it to the network
-        LOG.info("Deploy smart contract to network {}", nodeURL);
-        Transaction stx = Transaction.makeScriptTx(newAccount, bytecode, (byte)NETWORK_BYTE, fee);
-        String tx = node.send(stx);
-        LOG.info("Smart contract is deployed, transaction Id: {}", tx);
-
-        // time required for the transaction to be included into the blockchain
-        try {
-            Thread.sleep(10000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
         // Buyer make a transfer
-        Transaction tx1 = Transaction.makeTransferTx(newAccount, sellerAddress, 1,"WAVES", fee * 4 ,"WAVES", "Sending currency");
-        LOG.info("Create buy transaction detail: {}", tx1.getJson());
+        LOG.info("Buyer sell money and request for goods, example 10 waves");
+        Transaction buyTx = Transaction.makeTransferTx(buyer, smartAccount.getAddress(), 1000000000,"WAVES", fee * 4 ,"WAVES", "Sending Money");
+        String sendingTX = node.send(buyTx);
+        LOG.info("Transaction Id: {}", sendingTX);
+        
+        
+        LOG.info("Seller request withraw money");
+        Transaction tx1 = Transaction.makeTransferTx(smartAccount, sellerAddress, 1000000000,"WAVES", fee * 4 ,"WAVES", "Sending Money");
 
-        //Buyer and Escrow sign the deal with proofs
+        //Buyer and Seller sign the deal with proofs
         String buyerSig =  buyer.sign(tx1);
-        LOG.info("Buyer sign buy transaction {}", buyerSig);
-        String escrowSig = escrow.sign(tx1);
-        LOG.info("Esco sign buy transaction {}", buyerSig);
+        LOG.info("Buyer accept transaction by signning it: {}", buyerSig);
+        
+        String sellerSig =  seller.sign(tx1);
+        LOG.info("Seller sign with transaction: {}", sellerSig);
         tx1 = tx1.withProof(0, buyerSig);
-        tx1 = tx1.withProof(2, escrowSig);
+        tx1 = tx1.withProof(1, sellerSig);
+        
+        LOG.info("Transaction after signs: {}", tx1.getJson());
 
-        // Send the transfer transaction to the network and print the Tx Id
         String txid = node.send(tx1);
-        LOG.info("Send buy transaction Id : {}", txid);
+        LOG.info("Broadcast transaction to network transaction Id : {}", txid);
 	}
 }
